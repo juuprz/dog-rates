343480e719b2d7cab727053cae8db3f7da96ac7c
Julian Perez <juuprz@gmail.com>
2019-05-04 16:46:10 -0700

    Add working image uploader to cloudinary. Add preview functionality upon upload which provides scaled preview

--- client/src/components/Submit.jsx
+++ client/src/components/Submit.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 
-
 const CLOUDINARY_UPLOAD_PRESET = 'sx7elaqc';
 const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/juuprz/image/upload';
 
@@ -14,43 +13,56 @@ export default class Submit extends React.Component {
     };
     this.onDrop = this.onDrop.bind(this);
   }
-  onDrop(acceptedFiles) {
-    console.log(acceptedFiles);
+  onDrop(files) {
+    console.log(files);
+    this.setState({
+      uploadedFile: files[0]
+    });
+
+    this.handleImageUpload(files[0]);
+  }
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+      .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
   }
   render() {
-    return(
+    return (
     <div>
-        <Dropzone multiple={false} accept="image/*" onDrop={this.onDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      </div>
+      <div className='FileUpload'>
+          <Dropzone multiple={false} accept="image/*" onDrop={this.onDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
+        </div>
+        <div>
+          <div>
+            {this.state.uploadedFileCloudinaryUrl === '' ? null :
+              <div>
+                <p>{this.state.uploadedFile.name}</p>
+                <img src={this.state.uploadedFileCloudinaryUrl} style={{maxWidth: 300, maxHeight: 300}}/>
+              </div>}
+          </div>
+        </div>
+    </div>
     )
   }
 }
-
-// export default class Submit extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//     }
-//     this.onImageDrop = this.onImageDrop.bind(this);
-//   }
-//   onImageDrop() {
-//     console.log('item droppped');
-//   }
-//   render() {
-//     <Dropzone
-//       multiple={false}
-//       accept="image/*"
-//       onDrop={this.onImageDrop}>
-//       <p>Drop an image or click to select a file to upload.</p>
-//     </Dropzone>
-//   }
-// }
